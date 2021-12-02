d8ad83e02faaae8c222310dd77bb4d2242e723b8
Angel <mr.angelantoniolopez@gmail.com>
2021-12-01 22:59:13 -0600

    inicio

---
+++ app/src/main/java/sv/edu/usam/api_bomberos/MainActivity.java
@@ -0,0 +1,118 @@
+package sv.edu.usam.api_bomberos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email, contraseña;
+
+    String str_email,str_password;
+    //String url = "http://login.corporacionefransac.com/login.php";
+    String url = "https://apibomberos.000webhostapp.com/datos/usuario.php";
+    //String URL = "http://192.168.0.7/USAM_API/api/login.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.etemail);
+        contraseña = findViewById(R.id.etcontraseña);
+    }
+
+    public void Login(View view) {
+
+        if(email.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+        }
+        else if(contraseña.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Por favor espera...");
+
+            progressDialog.show();
+
+            str_email = email.getText().toString().trim();
+            str_password = contraseña.getText().toString().trim();
+
+
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
+
+                    if (!response.equalsIgnoreCase("{\"mensaje\":\"error\"}")) {
+                        JSONObject respuestaObj = new JSONObject();
+                        try {
+                            respuestaObj = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        email.setText("");
+                        contraseña.setText("");
+                        String nombre = "";
+                        try {
+                            nombre = respuestaObj.getString("nombre");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        startActivity(new Intent(getApplicationContext(), Inicio.class));
+                        Toast.makeText(MainActivity.this, "Bienvenido " + nombre, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("accion","login");
+                    params.put("correo",str_email);
+                    params.put("clave",str_password);
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+            requestQueue.add(request);
+        }
+    }
+
+    public void moveToRegistration(View view) {
+        startActivity(new Intent(getApplicationContext(), RegistroSesion.class));
+        finish();
+    }
+}
